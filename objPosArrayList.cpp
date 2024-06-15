3e27ad85fc94cee77591a90820a603c7aad0868a
Yucheng Chen 400478105 <cheny883@mcmaster.ca>
2024-06-15 16:36:10 -0400

    yuri

--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -4,3 +4,80 @@
 // Paste your CUTE Tested implementation here.
 // Paste your CUTE Tested implementation here.
 
+
+objPosArrayList::objPosArrayList() : sizeList(0), sizeArray(ARRAY_MAX_CAP) {
+    aList = new objPos[ARRAY_MAX_CAP];
+}
+
+objPosArrayList::~objPosArrayList() {
+    delete[] aList;
+}
+
+int objPosArrayList::getSize() {
+    return sizeList;
+}
+
+void objPosArrayList::insertHead(objPos thisPos) {
+    if (sizeList < ARRAY_MAX_CAP) {
+        for (int i = sizeList; i > 0; --i) { 
+            aList[i] = aList[i - 1]; 
+        }
+        aList[0] = thisPos; 
+        ++sizeList;
+    }
+}
+
+void objPosArrayList::removeHead() {
+    if (sizeList > 0) {
+        for (int i = 0; i < sizeList - 1; ++i) {
+            aList[i] = aList[i + 1];
+        }
+        --sizeList;
+    }
+}
+
+void objPosArrayList::insertTail(objPos thisPos) {
+    if (sizeList < ARRAY_MAX_CAP) {
+        aList[sizeList] = thisPos;
+        ++sizeList;
+    }
+}
+
+void objPosArrayList::removeTail() {
+    if (sizeList > 0) {
+        --sizeList;
+    }
+}
+
+void objPosArrayList::clear() {
+    while (getSize() > 0) {
+        removeTail();
+    }
+}
+
+void objPosArrayList::getHeadElement(objPos& returnPos) {
+    if (sizeList > 0) {
+        returnPos = aList[0];
+    }
+}
+
+void objPosArrayList::getTailElement(objPos& returnPos) {
+    if (sizeList > 0) {
+        returnPos = aList[sizeList - 1];
+    }
+}
+
+void objPosArrayList::getElement(objPos& returnPos, int index) {
+    if (index >= 0 && index < sizeList) {
+        returnPos = aList[index];
+    }
+}
+
+void objPosArrayList::removeElement(int index) {
+    if (index >= 0 && index < sizeList) {
+        for (int i = index; i < sizeList - 1; ++i) {
+            aList[i] = aList[i + 1]; 
+        }
+        sizeList--; 
+    }
+}
