e8b3519ffa39de3bc05a9ea2a9a18bd06c52bb1c
Yucheng Chen 400478105 <cheny883@mcmaster.ca>
2024-06-19 20:43:09 -0400

    yuri - some comments to help read the code

--- objPos.cpp
+++ objPos.cpp
@@ -1,5 +1,6 @@
 #include "objPos.h" 
 
+// Default constructor, initializes position to (0,0) and symbol to NULL
 objPos::objPos()
 {
     x = 0;
@@ -36,6 +37,7 @@ void objPos::setObjPos(int xPos, int yPos, char sym)
     symbol = sym;
 }
 
+// Gets the current position and symbol, stores in the provided objPos object
 void objPos::getObjPos(objPos &returnPos)
 {
     returnPos.setObjPos(x, y, symbol);
@@ -46,11 +48,14 @@ char objPos::getSymbol()
     return symbol;
 }
 
+
+// Checks if this position is equal to the given objPos reference
 bool objPos::isPosEqual(const objPos* refPos)
 {
     return (refPos->x == x && refPos->y == y);
 }
 
+// Returns the symbol if the position is equal to the given objPos reference, otherwise returns NULL
 char objPos::getSymbolIfPosEqual(const objPos* refPos)
 {
     if(isPosEqual(refPos))
--- objPosArrayList.cpp
+++ objPosArrayList.cpp
@@ -4,45 +4,51 @@
 // Paste your CUTE Tested implementation here.
 // Paste your CUTE Tested implementation here.
 
-
+// Constructor: initializes the list with a maximum capacity and sets the current size to 0
 objPosArrayList::objPosArrayList() : sizeList(0), sizeArray(ARRAY_MAX_CAP) {
     aList = new objPos[ARRAY_MAX_CAP];
 }
 
+// Destructor: deallocates the dynamically allocated array
 objPosArrayList::~objPosArrayList() {
     delete[] aList;
 }
 
+// Returns the current number of elements in the list
 int objPosArrayList::getSize() {
     return sizeList;
 }
 
+// Inserts an element at the head of the list, shifts other elements to the right
 void objPosArrayList::insertHead(objPos thisPos) {
-    if (sizeList < ARRAY_MAX_CAP) {
+    if (sizeList < ARRAY_MAX_CAP) {// Ensure there's space in the array
         for (int i = sizeList; i > 0; i--) { 
-            aList[i] = aList[i - 1]; 
+            aList[i] = aList[i - 1];  // Shift elements to the right
         }
-        aList[0] = thisPos; 
-        sizeList++;
+        aList[0] = thisPos; // Insert new element at the head
+        sizeList++;// Increment size of the list
     }
 }
 
+// Removes the element at the head of the list, shifts other elements to the left
 void objPosArrayList::removeHead() {
-    if (sizeList > 0) {
+    if (sizeList > 0) {// Ensure the list is not empty
         for (int i = 0; i < sizeList - 1; i++) {
-            aList[i] = aList[i + 1];
+            aList[i] = aList[i + 1];// Shift elements to the left
         }
         sizeList--;
     }
 }
 
+// Inserts an element at the tail of the list
 void objPosArrayList::insertTail(objPos thisPos) {
     if (sizeList < ARRAY_MAX_CAP) {
-        aList[sizeList] = thisPos;
+        aList[sizeList] = thisPos;// Insert new element at the tail
         sizeList++;
     }
 }
 
+// Removes the element at the tail of the list
 void objPosArrayList::removeTail() {
     if (sizeList > 0) {
         sizeList--;
@@ -57,31 +63,33 @@ void objPosArrayList::clear() {
 
 void objPosArrayList::getHeadElement(objPos& returnPos) {
     if (sizeList > 0) {
-        returnPos = aList[0];
+        returnPos = aList[0];// Return the head element
     }
 }
 
+// Retrieves the element at the tail of the list
 void objPosArrayList::getTailElement(objPos& returnPos) {
     if (sizeList > 0) {
-        returnPos = aList[sizeList - 1];
+        returnPos = aList[sizeList - 1];// Return the tail element
     }
 }
 
+// Retrieves the element at the specified index of the list
 void objPosArrayList::getElement(objPos& returnPos, int index) {
     if (index >= 0 && index < sizeList) {
         returnPos = aList[index];
     }
     else
     {
-        returnPos = objPos {-1, -1, '\0'};
+        returnPos = objPos {-1, -1, '\0'};// Return an invalid position if index is out of bounds
     }
 }
 
 void objPosArrayList::removeElement(int index) {
     if (index >= 0 && index < sizeList) {
         for (int i = index; i < sizeList - 1; i++) {
-            aList[i] = aList[i + 1]; 
+            aList[i] = aList[i + 1]; // Shift elements to the left
         }
-        sizeList--; 
+        sizeList--; // Decrement size of the list
     }
 }
